6061d29d32a3cb53a23115c775963472f8c13acb
ekambains <bainsinbusiness@gmail.com>
2025-01-14 16:13:00 -0500

    Testing Create User post requests

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import User from './models/user.model.js';
 
 dotenv.config();
 
@@ -8,8 +9,28 @@ const app = express();
 
 const PORT = 5000;
 
+app.use(express.json());
+
 app.get("/",(req, res) => {
-    res.send("<h1>Jatt</h1> \n <h1>Fuck Off</h1>");
+    res.send("<h1>Jatt</h1>");
+})
+
+app.post("/user", async(req, res) => {
+    const user = req.body;
+    if(!user.username || !user.email || !user.password) {
+        return res.status(400).json({success: false, message: "Please provide all fields"});
+    }
+
+    const newUser = new User(user);
+
+    try {
+        await newUser.save();
+        res.status(201).json({success: true, data: newUser});
+    }
+    catch(error) {
+        console.error("Error in Create User: ", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
 })
 
 app.listen(PORT, () => {
