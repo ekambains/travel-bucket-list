8542dbeb4152a87467f3005e0927b438f90ffeea
ekambains <bainsinbusiness@gmail.com>
2025-01-15 14:25:52 -0500

    User authentication  complete

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import User from './models/user.model.js';
 import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -14,18 +15,17 @@ app.use(express.json());
 
 app.get("/",(req, res) => {
     res.send("<h1>Jatt</h1>");
-})
+});
 
 app.post("/user", async(req, res) => {
-    const user = req.body;
-    if(!user.username || !user.email || !user.password) {
-        return res.status(400).json({success: false, message: "Please provide all fields"});
-    }
-    user.password =  await bcrypt.hash(user.password, 10);
-    console.log(user.password);
-    const newUser = new User(user);
-
     try {
+        const user = req.body;
+        if(!user.username || !user.email || !user.password) {
+            return res.status(400).json({success: false, message: "Please provide all fields"});
+        }
+        user.password =  await bcrypt.hash(user.password, 10);
+        console.log(user.password);
+        const newUser = new User(user);
         await newUser.save();
         res.status(201).json({success: true, data: newUser});
     }
@@ -33,7 +33,30 @@ app.post("/user", async(req, res) => {
         console.error("Error in Create User: ", error.message);
         res.status(500).json({success: false, message: "Server Error"});
     }
-})
+});
+
+app.post("/user/login", async(req, res) => {
+    try {
+        const user = await User.findOne({
+            $or: [{ username: req.body.username }, { email: req.body.username }]
+        });
+
+        if(!user) {
+            res.json({success: false, message: "User doesn't exist."});
+        }
+
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.json({success: true, message: "Login Successful"});
+        }
+        else {
+            res.json({success: false, message: "Incorrect Password"});
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({success: false, message: "An error occured during login."})
+    }
+});
 
 app.listen(PORT, () => {
     connectDB();
