e06632436a2f034ebe6ea5627acbf969d8fb7997
ekambains <bainseka@sheridancollege.ca>
2025-01-18 23:37:26 -0500

    Some changes to the structure.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import BucketList from './models/bucketList.model.js';
 import mongoose from 'mongoose';
+import authenticateToken from './middlewares/authenticateToken.js';
 
 dotenv.config();
 
@@ -61,18 +62,6 @@ app.post("/login", async(req, res) => {
     }
 });
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if(token == null) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    })
-}
 
 app.get("/api/bucket", authenticateToken, async(req, res) => {
     try {
